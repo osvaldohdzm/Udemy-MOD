593dd34a9a4d78ef0b4d5191561a4e03feff02cf
Your Name <you@example.com>
2022-07-14 15:44:06 -0500

    saved

--- background.js
+++ background.js
@@ -1,8 +1,3 @@
 let color = "#fa9639"
 
-
-// Run when the user first installs the extension
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({color}) // Use sync to store persistent data
-    console.log("Button Color Set")
-})
+console.log("background.js")
--- popup.js
+++ popup.js
@@ -1,40 +1,64 @@
-// Snag our button
-let btn = document.getElementById("changeFont")
-let clickExamBtn = document.getElementById("clickExam")
+let clickExamBtn = document.getElementById("clickExam");
 
-// Set our button's color to the color that we stored
-chrome.storage.sync.get("color", ({color}) => {
-    btn.style.backgroundColor = color
-})
-
-// Run on click
-btn.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({active: true, currentWindow:true}) // Find current tab
-
-    chrome.scripting.executeScript({ // Run the following script on our tab
-        target: {tabId: tab.id},
-        function: () => {
-            let elems = document.querySelectorAll("*"); // Grab every element in the dom
-            for (var i = 0;i < elems.length; i++){ 
-                elems[i].style.fontFamily = "Comic Sans MS";
-            }
+async function waitForElm(selector) {
+    console.log("funcion...")   ;
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
         }
-    })
-})
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+
+
+function countInArray(array, value) {
+    return array.reduce((n, x) => n + (x === value), 0);
+}
+
+async function main() {
+    var exams = document.querySelectorAll('div.quiz-editor--quiz-editor--2RKDC.default-item-editor--item-editor--3GhNq');
+    console.log("Examenes: " + exams.length);
+    
+    for (let j = 0; j < exams.length; j++) {
+        var preguntas = exams[j].querySelectorAll('div.assessment-list--assessment-list--1sF23 > ul > li')
+        console.log("Preguntas en examen " + (j + 1) + ": " + preguntas.length);
+        document.querySelectorAll('div.quiz-editor--quiz-editor--2RKDC.default-item-editor--item-editor--3GhNq > div:nth-child(1) > div > div.item-bar--right--4qb_w.item-bar--row--3ZgKF > button')[j].click();
+        var arr = [];
+        for (let i = 0; i < preguntas.length; i++) {                   
+            preguntas[i].querySelector('div.assessment-list--assessment-list--1sF23 > ul > li:nth-child(' + (i + 1) + ') > div > button:nth-child(5)').click();
+            var querySelector = 'input[type=radio][data-purpose="knowledge-area-option-toggle"]:checked';
+            console.log("HOLA1")  ;   
+            await waitForElm(querySelector);
+            console.log("HOLA2")    ; 
+            var selectedId = document.querySelector('input[type=radio][data-purpose="knowledge-area-option-toggle"]:checked').id;
+            const knowledge_area = document.getElementById(selectedId).parentElement.parentElement.querySelector('input[data-purpose="knowledge-area-option-input"]').value.toUpperCase().normalize();
+            arr.push(knowledge_area);
+            document.querySelectorAll('div.quiz-editor--quiz-editor--2RKDC.default-item-editor--item-editor--3GhNq > div:nth-child(1) > div > div.item-bar--right--4qb_w.item-bar--row--3ZgKF > button')[j].click();
+            console.log("Pregunta " + (i + 1)+ " : "+knowledge_area);
+        };
+        const count = {};
+        arr.forEach(e => count[e] ? count[e]++ : count[e] = 1 );
+        console.log(count);
+    };
+}
 
 clickExamBtn.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({active: true, currentWindow:true}) // Find current tab
-
-    chrome.scripting.executeScript({ // Run the following script on our tab
+    let [tab] = await chrome.tabs.query({active: true, currentWindow:true}) 
+    chrome.scripting.executeScript({ 
         target: {tabId: tab.id},
-        function: () => {
-            var exams = document.querySelectorAll('li[class*="s-curriculum-item-draggable curriculum-list--list-item--MB8ip curriculum-list--non-section-item--2ilUv"]');
-            console.log("Examenes: "+exams.length);
-            exams.forEach(element => {
-                console.log("Preguntas: "+ element.querySelectorAll('li[data-purpose*="assessment-list-item"]').length);
-                
-            });
+        files: ['popup.js'],
+       function:  main
+    }  );
 
-        }
-    })
 })
